29a8a10651dbb0ff789594e80e99cd099b9e146a
Bherna33 <brandenjhernandez@gmail.com>
2025-04-18 00:14:49 -0500

    fixed title

--- api/card.js
+++ api/card.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const playerInfo = await playerURL.json();
   
     const svg = `
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <svg width="420" height="200" xmlns="http://www.w3.org/2000/svg">
         <style>
           .default { fill: gray; }
           .label { font-weight: bold; }
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         <a href="${playerInfo.url}" target="_blank">
             <rect width="100%" height="100%" rx="15" ry="15" class="default" />
         </a>
-        <text style="font: bold 20px sans-serif; fill:black;" x="20" y="30">${username}'s Chess Profile</text>
+        <text style="font: bold 20px sans-serif; fill:black;" text-anchor="middle" x="210" y="30">${username}'s Chess Profile</text>
   
         <text x="20" y="60" class="label">Rapid:</text>
         <text x="80" y="60">${playerStats.chess_rapid?.last?.rating ?? "N/A"}</text>
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         <text x="20" y="160" class="label">League:</text>
         <text x="80" y="160">${playerInfo.league ?? "N/A"}</text>
   
-        <image width="75" height="75" x="170" y="65" href="${playerInfo.avatar ? await getBase64Image(playerInfo.avatar) : "" }" alt="avatar" />
+        <image width="100" height="100" x="230" y="55" href="${playerInfo.avatar ? await getBase64Image(playerInfo.avatar) : "" }" alt="avatar" />
       </svg>
     `;
   

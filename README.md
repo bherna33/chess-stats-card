f0eeb07ee66aa86f6cda74f836a8fa9f5f9e92f1
Bherna33 <brandenjhernandez@gmail.com>
2025-04-20 13:02:44 -0500

    Added Themes I updated the readme, and also added themes to the repo. All the themes are inspired from github readme stats

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# chess-stats-card
-A dynamically generated stats for chess.com
+<div align="center">
+    <h1>Chess Stats Card</h1>
+    Display your chess.com rating on your GitHub profile README.
+</div>
+
+## ⚡ Quick setup
+
+1. Star this repo 😁
+2. Place the code below in you repo and replace username with your own.
+```
+[![Chess stats card](https://chess-stats-card.vercel.app/api/card?user=useername)
+```
+
--- api/card.js
+++ api/card.js
@@ -2,51 +2,67 @@ const themes = require('./themes');
 
 export default async function handler(req, res) {
 
-    const { username = "magnuscarlsen", theme = "dark" } = req.query;
+    const { user = "magnuscarlsen", theme = "dark" } = req.query;
     const selectedTheme = themes[theme] || themes["dark"];
 
-  
-    const statsURL = await fetch(`https://api.chess.com/pub/player/${username}/stats`);
-    const playerURL = await fetch(`https://api.chess.com/pub/player/${username}`);
-  
+    const statsURL = await fetch(`https://api.chess.com/pub/player/${user}/stats`);
+    const playerURL = await fetch(`https://api.chess.com/pub/player/${user}`);
+
     const playerStats = await statsURL.json();
     const playerInfo = await playerURL.json();
-  
-    const svg = `
-      <svg width="420" height="200" xmlns="http://www.w3.org/2000/svg">
-        <style>
-          .default { fill: ${selectedTheme.background}; }
-          .label { font-weight: bold; }
-        </style>
 
-        <a href="${playerInfo.url}" target="_blank">
-            <rect width="100%" height="100%" rx="10" ry="10" class="default" />
-        </a>
-        <text style="font: bold 20px sans-serif; fill:black;" text-anchor="middle" x="210" y="30">${username}'s Chess Profile</text>
-  
-        <text x="20" y="60" class="label">Rapid:</text>
-        <text x="80" y="60">${playerStats.chess_rapid?.last?.rating ?? "N/A"}</text>
-  
-        <text x="20" y="85" class="label">Blitz:</text>
-        <text x="80" y="85">${playerStats.chess_blitz?.last?.rating ?? "N/A"}</text>
-  
-        <text x="20" y="110" class="label">Daily:</text>
-        <text x="80" y="110">${playerStats.chess_daily?.last?.rating ?? "N/A"}</text>
-  
-        <text x="20" y="135" class="label">Bullet:</text>
-        <text x="80" y="135">${playerStats.chess_bullet?.last?.rating ?? "N/A"}</text>
-  
-        <text x="20" y="160" class="label">League:</text>
-        <text x="80" y="160">${playerInfo.league ?? "N/A"}</text>
-  
-        <image width="100" height="100" x="230" y="55" href="${playerInfo.avatar ? await getBase64Image(playerInfo.avatar) : "" }" alt="avatar" />
-      </svg>
+    const svg = `
+        <svg width="420" height="200" xmlns="http://www.w3.org/2000/svg">
+            <style>
+                text{
+                    font-family: Segoe UI;
+                    fill:${selectedTheme.textColor};
+                }
+                .border {
+                    fill: ${selectedTheme.border};
+                }
+                .background {
+                    fill: ${selectedTheme.background};
+                }
+                .label {
+                    font-weight: bold;
+                }
+                .title{
+                    fill: ${selectedTheme.titleColor};
+                    font-size: 20px;
+                    font-weight: bold;
+                }
+            </style>
+
+            <a href="${playerInfo.url}" target="_blank">
+                <rect width="100%" height="100%" rx="5" ry="5" class="border" />
+                <rect width="417" height="197" rx="5" ry="5" x="1.5" y="1.5" class="background" />
+            </a>
+            <text class="title" text-anchor="middle" x="210" y="30">${user}'s Chess Profile</text>
+
+            <text x="20" y="60" class="label">Rapid:</text>
+            <text x="100" y="60">${playerStats.chess_rapid?.last?.rating ?? "N/A"}</text>
+
+            <text x="20" y="85" class="label">Blitz:</text>
+            <text x="100" y="85">${playerStats.chess_blitz?.last?.rating ?? "N/A"}</text>
+
+            <text x="20" y="110" class="label">Daily:</text>
+            <text x="100" y="110">${playerStats.chess_daily?.last?.rating ?? "N/A"}</text>
+
+            <text x="20" y="135" class="label">Bullet:</text>
+            <text x="100" y="135">${playerStats.chess_bullet?.last?.rating ?? "N/A"}</text>
+
+            <text x="20" y="160" class="label">League:</text>
+            <text x="100" y="160">${playerInfo.league ?? "N/A"}</text>
+
+            <image width="100" height="100" x="230" y="55" href="${playerInfo.avatar ? await getBase64Image(playerInfo.avatar) : "" }" alt="avatar" />
+        </svg>
     `;
-  
+
     res.setHeader("Content-Type", "image/svg+xml");
     res.setHeader("Cache-Control", "public, max-age=0, must-revalidate");
     res.status(200).send(svg);
-  }
+}
 
 async function getBase64Image(url) {
     const response = await fetch(url);
